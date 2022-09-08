ae512df5879cccc2f4ef32f4e75b65eecc46ca14
Abigail Harrison <abigail@giffgaff.co.uk>
2022-09-08 15:46:48 +0100

    maint: Remove .next and node_modules folders

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/use-transition":["static\u002Fchunks\u002Fpages\u002Fuse-transition.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fuse-transition"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- pages/use-debug-value.js
+++ pages/use-debug-value.js
@@ -1,4 +1,4 @@
-import React, { useDebugValue } from 'react';
+import React, { useState, useDebugValue } from 'react';
 import Layout from '../components/layout';
 
 export default function DebugValue() {
@@ -6,8 +6,37 @@ export default function DebugValue() {
 		<Layout>
 			<h1>useDebugValue</h1>
 			<p>
-				useDebugValue can be used to display a label for custom hooks in React
-				DevTools.
+				useDebugValue can be used to print out debug information in the console
+				for custom hooks.
+			</p>
+			<p>
+				<code>useDebugValue</code> can be thought of as a{' '}
+				<code>console.log()</code> for React custom hooks.
+			</p>
+			<p>
+				<code>useDebugEffect</code> cannot be rendered conditionally due to the
+				Hooks rules preventing Hooks being called conditionally.
+			</p>
+			<p>
+				Using <code>useDebugValue</code> heavily in production code might
+				negatively impact the performance of your app.
+			</p>
+			<p>Don't leave it in your production code</p>
+			<h2>Example code</h2>
+			<p>
+				<code>{'export default function useUser() {'}</code>
+			</p>
+			<p>
+				<code>{'const [user, setUser] = useState(getUser());'}</code>
+			</p>
+			<p>
+				<code>{'useDebugValue(user == null ? "No User" : user.name);'}</code>
+			</p>
+			<p>
+				<code>{'return [user, setUser];'}</code>
+			</p>
+			<p>
+				<code>{'}'}</code>
 			</p>
 		</Layout>
 	);
